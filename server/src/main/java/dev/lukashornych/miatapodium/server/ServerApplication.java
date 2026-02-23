b32067be79d4d9fab43bfcec2399b6a8be7dacf5
Lukáš Hornych <lukashornych@proton.me>
2026-02-23 21:00:22 +0100

    feat: server base spring boot project

---
+++ server/src/main/java/dev/lukashornych/miatapodium/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package dev.lukashornych.miatapodium.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
